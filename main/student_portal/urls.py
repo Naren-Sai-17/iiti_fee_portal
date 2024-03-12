673edd7d957a9597641967ff32f2e3f3989f57f7
Naren-Sai-17 <narenkumarsai2005@gmail.com>
2024-03-12 16:37:11 +0530

    auth update

--- main/student_portal/urls.py
+++ main/student_portal/urls.py
@@ -5,5 +5,6 @@ app_name = "student_portal" # refer namespacing in django
 
 urlpatterns = [
     path("",views.index, name = "index"),
-    path("dashboard",views.dashboard, name = "dashboard")
+    path("dashboard",views.dashboard, name = "dashboard"),
+    path("logout",views.LogoutView.as_view(), name = "logout"),
 ]
--- main/student_portal/views.py
+++ main/student_portal/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse , HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth import logout as django_logout
+from allauth.account.views import LogoutView 
 
 # Create your views here.
 def index(request): 
     if request.user.is_authenticated: 
         return HttpResponseRedirect(reverse("student_portal:dashboard"))
-    print("here")
     return render(request, "login.html")
 def dashboard(request): 
     return render(request, "dashboard.html")
+
