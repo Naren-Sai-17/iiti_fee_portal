45f184302fe7173fb9fdb953d7b8944d3a72c2e9
Naren-Sai-17 <narenkumarsai2005@gmail.com>
2024-04-12 15:42:51 +0530

    debug mode

--- main/main/settings.py
+++ main/main/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env('.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com','localhost','127.0.0.1','iiti-fee-portal.onrender.com']
 
